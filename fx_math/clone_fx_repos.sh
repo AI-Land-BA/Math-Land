39c2186054fc83bf580a7364d0acfad8f144bc50
Brian Abbott <briancabbott@gmail.com>
2019-10-06 18:26:17 -0600

    updating

---
+++ fx_math/clone_fx_repos.sh
@@ -0,0 +1,10 @@
+
+git clone http://gitbox.apache.org/repos/asf/commons-math.git
+
+mkdir MathJax
+cd MathJax
+git clone https://github.com/mathjax/MathJax.git
+cd ..
+
+
+git clone https://github.com/google/s2geometry.git
